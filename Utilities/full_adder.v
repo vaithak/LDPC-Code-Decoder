a04325f74f174154ec0d47fe791c8c2c322cae06
Mshivam2409 <shivammalhotra08@gmail.com>
2020-07-04 00:43:29 +0530

    Add Modules

---
+++ Utilities/full_adder.v
@@ -0,0 +1,27 @@
+module full_adder 
+  (
+   i_bit1,
+   i_bit2,
+   i_carry,
+   o_sum,
+   o_carry
+   );
+ 
+  input  i_bit1;
+  input  i_bit2;
+  input  i_carry;
+  output o_sum;
+  output o_carry;
+ 
+  wire   w_WIRE_1;
+  wire   w_WIRE_2;
+  wire   w_WIRE_3;
+       
+  assign w_WIRE_1 = i_bit1 ^ i_bit2;
+  assign w_WIRE_2 = w_WIRE_1 & i_carry;
+  assign w_WIRE_3 = i_bit1 & i_bit2;
+ 
+  assign o_sum   = w_WIRE_1 ^ i_carry;
+  assign o_carry = w_WIRE_2 | w_WIRE_3;
+   
+endmodule // full_adder
