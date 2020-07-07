daff3471376f95ee9aa90aed4be8be6f5bea8ae7
Sreekar Malkurthi <54896101+Sreekar-M@users.noreply.github.com>
2020-07-07 15:58:09 +0530

    Create AG module

--- address_generator/AG.v
+++ address_generator/AG.v
@@ -1,1 +1,16 @@
-ECHO is on.
+module AG #
+(
+  parameter NUM_BITS = 8, //number of bits in the counter
+  parameter COUNT_FROM = 0 //point where counting starts
+)
+(
+  input  logic                    clk, //clock
+  input  logic                    enable, //module enabling signal
+  input  logic                    reset, //reset input
+  output logic [NUM_BITS - 1 : 0] address //the address generated
+);
+  always @ (posedge clk) begin
+    if(reset && enable) address <= COUNT_FROM;
+    else if(enable) address <= address + 1;
+  end
+endmodule
