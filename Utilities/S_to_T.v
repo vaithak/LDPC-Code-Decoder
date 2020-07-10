1b03d3c4413e52151f733afd78260c9f442fe154
pranabp-bit <58358551+pranabp-bit@users.noreply.github.com>
2020-07-10 11:41:28 +0530

    Update S_to_T.v

--- Utilities/S_to_T.v
+++ Utilities/S_to_T.v
@@ -1,22 +1,32 @@
-// Convert number from Signed format to Two's complement
+/*-------------------------------------------------------------------------------
+This module is a part of VNU which converts the input check-to-variable extrinsic messages
+, which are represented in sign-magnitude format, to two's complement format to perform 
+additions on them.
+--------------------------------------------------------------------------------*/
 
 module S_to_T #(
-  parameter DATA_WIDTH = 5
+  parameter DATA_WIDTH = 5             
 ) 
+  //port declarations
 (
+  //input port
   input wire  [DATA_WIDTH - 1 : 0] inp,
+  
+  //output port
   output wire [DATA_WIDTH - 1 : 0] out
 );
+  
+  //register to store values assigned in the procedural block
+  reg [DATA_WIDTH - 1 : 0] x;      
 
-reg [DATA_WIDTH - 1 : 0] x;
-
+//procedural block  
 always @* begin
-  if (inp[DATA_WIDTH-1] == 1'b1)
-    x = -inp[DATA_WIDTH-2:0];
+  if (inp[DATA_WIDTH-1] == 1'b1)    //if number is negative:
+    x = -inp[DATA_WIDTH-2:0];       //two's complement of all the bits except the sign bit
   else
-    x = inp;
+    x = inp;                        //representation of a positive number is same in both formats
 end
 
-assign out = x;
+assign out = x;                     //outputs the value stored in register x 
 
 endmodule
