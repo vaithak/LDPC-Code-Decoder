39d0ecf42fecfc9c0e5051d53da83f6d4fcd5a73
pranabp-bit <58358551+pranabp-bit@users.noreply.github.com>
2020-07-11 12:37:50 +0530

    Update PI_1_unshuffle_tb.v

--- permutations/testbenches/PI_1_unshuffle_tb.v
+++ permutations/testbenches/PI_1_unshuffle_tb.v
@@ -1,7 +1,7 @@
 //testbench for module PI_1_unshuffle
 /*
 here random 6 bit number is sent as input whose value is then compared with the output
-which is connected to that input according to the condition of Pi 1 shuffle
+which is connected to that input according to the condition of Pi 1 unshuffle
 relation between a connected input_index and output_index is : input_index= (6*output_index)%35 
 for example: input[6]==output[(6*6)%35=1]
 NOTE: input[35]=output[35]
