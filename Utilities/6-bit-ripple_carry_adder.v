1bf899abfdf4a53c7f7dc41b65e7e0bd0146746a
Prateek Gupta <64007722+Prateek219@users.noreply.github.com>
2020-07-11 12:06:41 +0530

    Update 6-bit-ripple_carry_adder.v

--- Utilities/6-bit-ripple_carry_adder.v
+++ Utilities/6-bit-ripple_carry_adder.v
@@ -1,16 +1,16 @@
 // 6-bit ripple-carry adder
 
 module ripple_adder(
-  input [5:0] X,
+  input [5:0] X,         //6 bit input X,Y,S is defined
   input [5:0] Y,
-
+  input C_in,           //single bit C_in ,C_out is also defined 
   output [5:0] S,
   output C_out
   );
 
- wire w1, w2, w3, w4, w5;
+ wire w1, w2, w3, w4, w5;// wire is used to act as output from fulladder[i] to input in fulladder[i+1] 
 
- fulladder u1(X[0], Y[0], 1'b0, S[0], w1);
+ fulladder u1(X[0], Y[0], C_in, S[0], w1);
  fulladder u2(X[1], Y[1], w1, S[1], w2);
  fulladder u3(X[2], Y[2], w2, S[2], w3);
  fulladder u4(X[3], Y[3], w3, S[3], w4);
