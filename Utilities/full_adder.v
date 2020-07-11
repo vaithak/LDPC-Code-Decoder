a2c641b88240fb232005acb35eae1678dae137d5
Prateek Gupta <64007722+Prateek219@users.noreply.github.com>
2020-07-11 12:35:55 +0530

    Update full_adder.v

--- Utilities/full_adder.v
+++ Utilities/full_adder.v
@@ -2,17 +2,17 @@
 
 module fulladder(
   input X,
-  input Y, 
+  input Y,                //1 bit input define X,C_in,Y
   input C_in, 
 
-  output S, 
+  output S,           //1 bit output define S,C_out
   output C_out
   );
 
   wire w1,w2,w3;
 
   // Computing Sum (S)
-  xor gate_1(w1, X, Y);
+  xor gate_1(w1, X, Y);            //xor,or and and GATE with input a and output b,c when defined as XOR/AND/OR gate(a,b,c)
   xor gate_2(S, w1, C_in);
 
   // Computing Carry (C_out)
