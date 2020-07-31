85f7557b17484c162e0dbdc915a1a1a471ac443d
Sreekar Malkurthi <54896101+Sreekar-M@users.noreply.github.com>
2020-07-31 20:33:01 +0530

    Update LUT_tb.v

--- Utilities/testbenches/LUT_tb.v
+++ Utilities/testbenches/LUT_tb.v
@@ -1,5 +1,6 @@
+`include "LUT.v"
 module LUTTest;
-  reg [6-1 : 0] X;
+  reg [7-1 : 0] X;
   wire [4-1 : 0] Y;
 
   LUT test_module
@@ -12,7 +13,7 @@ module LUTTest;
   initial begin
     $dumpvars(0, LUTTest);
 
-    for(i = 0; i < (1 << 6); i = i + 1) begin
+    for(i = 0; i < (1 << 7); i = i + 1) begin
       X = i;
       #10;
       $display("%b %b", X, Y);
