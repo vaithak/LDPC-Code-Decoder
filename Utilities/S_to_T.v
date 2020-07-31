219f9809a83470e821854da3d7a113dff9d3c841
Sreekar Malkurthi <54896101+Sreekar-M@users.noreply.github.com>
2020-07-31 19:29:40 +0530

    Update S_to_T.v

--- Utilities/S_to_T.v
+++ Utilities/S_to_T.v
@@ -1,34 +1,32 @@
 /*-------------------------------------------------------------------------------
 This module converts the input, represented in sign-magnitude format, to two's complement format.
-For example: input = 100110  
+For example: input = 100110
              the first bit of the input being one indicates that it is a negative number.
              the output in this case will be the negative(two's complement) of 000110 (absolute value of input) which is 111010.
              output= 111010
 --------------------------------------------------------------------------------*/
 
-module S_to_T #(
-  parameter DATA_WIDTH = 5             
-) 
+module S_to_T
   //port declarations
 (
   //input port
-  input wire  [DATA_WIDTH - 1 : 0] inp,
-  
+  input wire  [5-1 : 0] inp,
+
   //output port
-  output wire [DATA_WIDTH - 1 : 0] out
+  output wire [5-1 : 0] out
 );
-  
+
   //register to store values assigned in the procedural block
-  reg [DATA_WIDTH - 1 : 0] x;      
+  reg [6-1 : 0] x;
 
-//procedural block  
-always @* begin
-  if (inp[DATA_WIDTH-1] == 1'b1)    //if number is negative:
-    x = -inp[DATA_WIDTH-2:0];       //two's complement of all the bits except the sign bit
-  else
-    x = inp;                        //representation of a positive number is same in both formats
-end
+  //procedural block
+  always @* begin
+    if (inp[4] == 1'b1)    //if number is negative:
+      x = ~{1'b0, inp[3:0]} + 1;       //two's complement of all the bits except the sign bit
+    else
+      x = {1'b0, inp};                        //representation of a positive number is same in both formats
+  end
 
-assign out = x;                     //outputs the value stored in register x 
+  assign out = x;                     //outputs the value stored in register x
 
 endmodule
